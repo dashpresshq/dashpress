5233c8fd44a3e2d842fea82a3b20ff2d29c78642
Ayobami Akingbade <akingbadefred@gmail.com>
2022-08-19 23:24:41 +0100

    test: entities list

---
+++ src/__tests__/api/entities/list.spec.ts
@@ -0,0 +1,39 @@
+import handler from "pages/api/entities/list";
+import { createAuthenticatedMocks } from "__tests__/helpers";
+import { setupAllTestData } from "__tests__/setup-test-data";
+
+describe("/api/entities/list", () => {
+  beforeAll(async () => {
+    await setupAllTestData();
+  });
+
+  it("should list all entities", async () => {
+    const { req, res } = createAuthenticatedMocks({
+      method: "GET",
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "label": "foo",
+          "value": "foo",
+        },
+        Object {
+          "label": "bar",
+          "value": "bar",
+        },
+        Object {
+          "label": "baz",
+          "value": "baz",
+        },
+        Object {
+          "label": "fish",
+          "value": "fish",
+        },
+      ]
+    `);
+  });
+});

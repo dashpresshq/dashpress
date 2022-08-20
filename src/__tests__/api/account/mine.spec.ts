bfee71e018f1a1416ca7b48d1a4a9d5a384c2205
Ayobami Akingbade <akingbadefred@gmail.com>
2022-08-20 03:47:43 +0100

    test: /api/account/min

---
+++ src/__tests__/api/account/mine.spec.ts
@@ -0,0 +1,58 @@
+import { setupAllTestData } from "__tests__/setup-test-data";
+import handler from "pages/api/account/mine";
+import { createAuthenticatedMocks } from "__tests__/helpers";
+
+describe("/api/account/mine", () => {
+  beforeAll(async () => {
+    await setupAllTestData(["users"]);
+  });
+
+  it("should get authenticated user profile", async () => {
+    const { req, res } = createAuthenticatedMocks({
+      method: "GET",
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+
+    expect(res._getJSONData()).toMatchInlineSnapshot(`
+      Object {
+        "name": "Root User",
+        "permissions": Array [],
+        "role": "creator",
+        "systemProfile": "{\\"userId\\": \\"1\\"}",
+        "username": "root",
+      }
+    `);
+  });
+
+  it("should update authenticated user profile", async () => {
+    const postRequest = createAuthenticatedMocks({
+      method: "PATCH",
+      body: {
+        name: "Updated Name",
+      },
+    });
+
+    await handler(postRequest.req, postRequest.res);
+
+    expect(postRequest.res._getStatusCode()).toBe(200);
+
+    const getRequest = createAuthenticatedMocks({
+      method: "GET",
+    });
+
+    await handler(getRequest.req, getRequest.res);
+
+    expect(getRequest.res._getJSONData()).toMatchInlineSnapshot(`
+      Object {
+        "name": "Updated Name",
+        "permissions": Array [],
+        "role": "creator",
+        "systemProfile": "{\\"userId\\": \\"1\\"}",
+        "username": "root",
+      }
+    `);
+  });
+});

1164a8277594c3d49adb93ea0ed2e0cb6346ce88
Ayobami Akingbade <akingbadefred@gmail.com>
2022-08-20 11:41:26 +0100

    test: User setup test

---
+++ src/__tests__/api/setup/user.spec.ts
@@ -0,0 +1,63 @@
+import handler from "pages/api/setup/user";
+import { setupUsersTestData } from "__tests__/setup-test-data/_users";
+import { createMocks } from "node-mocks-http";
+import { authTokenService } from "backend/lib/auth-token/auth-token.service";
+
+describe("/api/setup/user", () => {
+  beforeAll(async () => {
+    await setupUsersTestData(false);
+  });
+
+  it("should set up first user successfully", async () => {
+    const postRequest = createMocks({
+      method: "POST",
+      body: {
+        username: "newsetupuser",
+        name: "New Setup User",
+        password: "password",
+      },
+    });
+
+    await handler(postRequest.req, postRequest.res);
+
+    expect(postRequest.res._getStatusCode()).toBe(201);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { exp, iat, ...decodedToken } = await authTokenService.verify(
+      postRequest.res._getJSONData().token
+    );
+
+    expect(decodedToken).toMatchInlineSnapshot(`
+      Object {
+        "name": "New Setup User",
+        "role": "creator",
+        "username": "newsetupuser",
+      }
+    `);
+  });
+
+  it("should block request if users already exists", async () => {
+    const postRequest = createMocks({
+      method: "POST",
+      body: {
+        username: "anothernewsetupuser",
+        name: "New Setup User",
+        password: "password",
+      },
+    });
+
+    await handler(postRequest.req, postRequest.res);
+
+    expect(postRequest.res._getStatusCode()).toBe(400);
+
+    expect(postRequest.res._getJSONData()).toMatchInlineSnapshot(`
+      Object {
+        "message": "Primary user already setup",
+        "method": "POST",
+        "name": "BadRequestError",
+        "path": "",
+        "statusCode": 400,
+      }
+    `);
+  });
+});

78c061e3d79c88efe3641e8463529903fb91ca69
Ayobami Akingbade <akingbadefred@gmail.com>
2022-08-20 11:24:05 +0100

    test: Permissions tests

---
+++ src/__tests__/api/roles/[roleId]/permissions.spec.ts
@@ -0,0 +1,90 @@
+import handler from "pages/api/roles/[roleId]/permissions";
+import { createAuthenticatedMocks } from "__tests__/helpers";
+import { setupRolesTestData } from "__tests__/setup-test-data/_roles";
+
+describe("/api/roles/[roleId]/permissions", () => {
+  beforeAll(async () => {
+    await setupRolesTestData([
+      {
+        id: "some-admin-permissions",
+        permissions: ["CAN_RESET_PASSWORD", "CAN_MANAGE_PERMISSIONS"],
+      },
+    ]);
+  });
+
+  it("should get role permissions", async () => {
+    const { req, res } = createAuthenticatedMocks({
+      method: "GET",
+      query: {
+        roleId: "some-admin-permissions",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toEqual([
+      "CAN_RESET_PASSWORD",
+      "CAN_MANAGE_PERMISSIONS",
+    ]);
+  });
+
+  it("should remove permission from role", async () => {
+    const deleteRequest = createAuthenticatedMocks({
+      method: "DELETE",
+      query: {
+        roleId: "some-admin-permissions",
+      },
+      body: {
+        permission: "CAN_RESET_PASSWORD",
+      },
+    });
+
+    await handler(deleteRequest.req, deleteRequest.res);
+
+    expect(deleteRequest.res._getStatusCode()).toBe(204);
+
+    const { req, res } = createAuthenticatedMocks({
+      method: "GET",
+      query: {
+        roleId: "some-admin-permissions",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toEqual(["CAN_MANAGE_PERMISSIONS"]);
+  });
+
+  it("should add permission to role", async () => {
+    const postRequest = createAuthenticatedMocks({
+      method: "POST",
+      query: {
+        roleId: "some-admin-permissions",
+      },
+      body: {
+        permission: "CAN_CONFIGURE_APP",
+      },
+    });
+
+    await handler(postRequest.req, postRequest.res);
+
+    expect(postRequest.res._getStatusCode()).toBe(201);
+
+    const { req, res } = createAuthenticatedMocks({
+      method: "GET",
+      query: {
+        roleId: "some-admin-permissions",
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toEqual([
+      "CAN_MANAGE_PERMISSIONS",
+      "CAN_CONFIGURE_APP",
+    ]);
+  });
+});

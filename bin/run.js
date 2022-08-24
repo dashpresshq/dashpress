2dcc3f881d4b79d4b9b4aa60285b50afc028f669
Ayobami Akingbade <akingbadefred@gmail.com>
2022-08-24 04:33:24 +0100

    build: Braving @hadmean/run

---
+++ bin/run.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log("Hello");
+
+/*
+copy all the files to folder
+create config-data
+create .env.local
+
+npm install
+npm start
+*/
--- src/cli/index.ts
+++
@@ -1,3 +0,0 @@
-console.log("I am running");
-
-export {};

c0d91fb64f5eb6ecce3040a0c3d26d2a6275ff3c
Ayobami Akingbade <akingbadefred@gmail.com>
2022-10-13 22:50:36 +0100

    test: _app.tsx

---
+++ src/__tests__/_app.spec.tsx
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import MyApp from "pages/_app";
+
+describe("pages/users", () => {
+  const useRouter = jest.spyOn(require("next/router"), "useRouter");
+
+  function Foo({ title }: { title: string }) {
+    return <p>{title}</p>;
+  }
+
+  it("should render components", async () => {
+    useRouter.mockImplementation(() => ({
+      asPath: "/",
+    }));
+    // eslint-disable-next-line react/jsx-no-bind
+    render(<MyApp Component={Foo} pageProps={{ title: "Hello" }} />);
+
+    expect(screen.getByText("Hello")).toBeInTheDocument();
+  });
+});

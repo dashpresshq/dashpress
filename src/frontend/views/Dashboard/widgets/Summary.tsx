09b986537668fae3a7e3ccbcca0274702351fc00
Ayobami Akingbade <akingbadefred@gmail.com>
2022-10-28 23:34:06 +0100

    new summary design

--- src/frontend/views/Dashboard/widgets/Summary.tsx
+++ src/frontend/views/Dashboard/widgets/Summary.tsx
@@ -1,7 +1,8 @@
-import { Spacer, StyledCard, Text } from "@hadmean/chromista";
+import { Spacer, Stack, StyledCard, Text } from "@hadmean/chromista";
 import { useEntityConfiguration } from "frontend/hooks/configuration/configuration.store";
 import { useEntityFilterCount } from "frontend/hooks/data/data.store";
 import { ITableTab, QueryFilter } from "shared/types/data";
+import { ArrowDownLeft, ArrowUpRight, Users } from "react-feather";
 import { ISummaryWidgetConfig } from "shared/types/dashboard";
 import styled from "styled-components";
 import { abbreviateNumber } from "frontend/lib/numbers";
@@ -16,6 +17,22 @@ interface IProps {
   config: ISummaryWidgetConfig;
   setting?: IWidgetSetting;
 }
+
+export const SYSTEM_COLORS = {
+  danger: "#f5325c",
+  black: "#000",
+  success: "#03d87f",
+};
+
+const IconRoot = styled.div`
+  background: #e291822a;
+  color: #e29182;
+  width: 64px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export function SummaryWidget({ config, setting }: IProps) {
   const { queryId, entity } = config;
@@ -37,13 +54,29 @@ export function SummaryWidget({ config, setting }: IProps) {
   return (
     <StyledCard>
       <StyledBox>
-        <WidgetHeader setting={setting} config={config} />
-        <Spacer size="xs" />
-        <Text size="3" weight="bold">
-          {count?.isLoading
-            ? "Counting..."
-            : abbreviateNumber(count?.data?.count || 0)}
-        </Text>
+        <Stack spacing={18}>
+          <IconRoot>
+            <Users />
+          </IconRoot>
+          <div style={{ width: "100%" }}>
+            <WidgetHeader setting={setting} config={config} />
+            <Spacer size="xs" />
+            <Stack justify="space-between" align="end">
+              <Text size="3" weight="bold">
+                {count?.isLoading
+                  ? "Counting..."
+                  : abbreviateNumber(count?.data?.count || 0)}
+              </Text>
+              <Text size="6">
+                <ArrowUpRight
+                  size={16}
+                  style={{ color: SYSTEM_COLORS.success }}
+                />
+                <span style={{ color: SYSTEM_COLORS.success }}>34.45%</span>
+              </Text>
+            </Stack>
+          </div>
+        </Stack>
       </StyledBox>
     </StyledCard>
   );

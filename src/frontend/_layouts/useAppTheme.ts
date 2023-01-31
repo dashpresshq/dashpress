0f4284bc23feb1af079e9e2a0236c74b02270b30
Ayobami Akingbade <akingbadefred@gmail.com>
2023-01-31 12:28:05 +0100

    💎 style(theme): eBI theme work

--- src/frontend/_layouts/useAppTheme.ts
+++ src/frontend/_layouts/useAppTheme.ts
@@ -1,10 +1,54 @@
 import { useAppConfiguration } from "frontend/hooks/configuration/configuration.store";
-import { useTheme } from "@hadmean/chromista";
 import { useAuthenticatedUserPreferences } from "frontend/hooks/auth/user.store";
+import { IColorMode } from "@hadmean/chromista";
+import { useEffect } from "react";
+import { IRootColors } from "@hadmean/chromista/dist/theme/types";
+import { colorModeImplementation } from "./ebi";
 
 export type IThemeSettings = {
   primary: string;
   primaryDark: string;
+};
+
+export const DEFAULT_PRIMARY_COLOR = "#4b38b3";
+
+export const REPLACE_WITH_PRIMARY = "REPLACE_WITH_PRIMARY";
+
+export const prefixVarNameSpace = (color: string) => `--hadmean-${color}`;
+
+export const USE_ROOT_COLOR = (color: IRootColors) => {
+  return `var(${prefixVarNameSpace(color)})`;
+};
+
+export const generateRootColors = (
+  primaryColor: string,
+  colorMode: IColorMode
+): Record<string, string> => {
+  const rootColors: Record<string, string> = {};
+
+  rootColors[prefixVarNameSpace("primary-color")] = primaryColor;
+  rootColors[prefixVarNameSpace("primary-shade-color")] =
+    primaryColor + colorMode["shade-opacity"];
+
+  Object.entries(colorMode).forEach(([key, value]) => {
+    rootColors[prefixVarNameSpace(key)] =
+      value === REPLACE_WITH_PRIMARY ? primaryColor : value;
+  });
+  return rootColors;
+};
+
+export const useTheme = (
+  themeColor?: string,
+  colorMode?: "light" | "dark" | IColorMode
+) => {
+  useEffect(() => {
+    const primaryColor = themeColor || DEFAULT_PRIMARY_COLOR;
+    Object.entries(
+      generateRootColors(primaryColor, colorModeImplementation)
+    ).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    });
+  }, [themeColor, colorMode]);
 };
 
 export const useAppTheme = () => {
@@ -13,8 +57,5 @@ export const useAppTheme = () => {
 
   const theme = userPreferences.data?.theme || "light";
 
-  useTheme(
-    theme === "dark" ? themeColor.data?.primaryDark : themeColor.data?.primary,
-    theme
-  );
+  useTheme(themeColor.data?.primary, theme);
 };

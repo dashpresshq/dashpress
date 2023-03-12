98eb1b0c71eaf418051750c919bae2b345b2d2f5
Ayobami Akingbade <akingbadefred@gmail.com>
2023-03-12 15:45:38 +0100

    ♻️ refactor(color-shade): remove document dependent color shape impl

--- src/frontend/_layouts/useAppTheme.ts
+++ src/frontend/_layouts/useAppTheme.ts
@@ -1,9 +1,6 @@
 import { useAppConfiguration } from "frontend/hooks/configuration/configuration.store";
 import { IColorMode, useTheme } from "@hadmean/chromista";
 import { useAuthenticatedUserPreferences } from "frontend/hooks/auth/user.store";
-import { IRootColors } from "@hadmean/chromista/dist/theme/types";
-import { darken } from "polished";
-import { useCallback } from "react";
 import { processThemeColors } from "./portal";
 import { IThemeSettings } from "./types";
 import { getThemePrimaryColor } from "./utils";
@@ -23,20 +20,4 @@ export const useUserThemePreference = () => {
 export const useAppTheme = () => {
   const { primaryColor, theme } = useUserThemePreference();
   useTheme(primaryColor, theme);
-};
-
-export const prefixVarNameSpace = (color: string) => `--hadmean-${color}`;
-
-export const useThemeColorShade = () => {
-  const { primaryColor, theme } = useUserThemePreference();
-
-  return useCallback(
-    (colorKey: IRootColors, percent: number) => {
-      const color$1 = document.documentElement.style.getPropertyValue(
-        prefixVarNameSpace(colorKey)
-      );
-      return darken(`${percent / 100}`, color$1);
-    },
-    [typeof document, primaryColor, theme]
-  );
 };

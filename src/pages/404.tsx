9a2866de945721e1b795e593abb2a6ca6faef244
KyuubiTila <114122136+KyuubiTila@users.noreply.github.com>
2024-05-02 11:11:57 +0100

    ♻️ refactor(error page): styling using styled components

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,82 +1,86 @@
-import Link from "next/link";
-import React from "react";
+import styled from 'styled-components';
+import dynamic from 'next/dynamic';
 
-function Custom404() {
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  margin-top: 2rem;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  max-width: 400px;
+  display: block;
+  margin: 0 auto;
+`;
+
+const Heading = styled.h1`
+  font-size: 4.25rem;
+  font-weight: bold;
+  color: #333;
+  margin-top: 3rem;
+  font-family: 'Marker Felt', Fantasy;
+  margin-bottom: 1rem;
+`;
+
+const SubHeading = styled.p`
+  font-size: 1.7rem;
+  color: #333;
+  font-weight: bold;
+  font-family: 'Marker Felt', Fantasy;
+`;
+
+const StyledLink = styled.a`
+  font-size: 1rem;
+  display: inline-block;
+  background-color: #007bff;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  transition: background-color 0.3s ease;
+  margin-top: 1rem;
+  text-decoration: none;
+  font-family: 'Marker Felt', Fantasy;
+`;
+
+function Custom404(): JSX.Element {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "1rem",
-        }}
-      >
-        <div style={{ position: "relative", width: "100%", marginTop: "2rem" }}>
-          <img
+    <Container>
+      <Content>
+        <ImageContainer>
+          <Image
             src="https://merakiui.com/images/components/illustration.svg"
             alt=""
-            style={{
-              width: "100%",
-              maxWidth: "400px",
-              display: "block",
-              margin: "0 auto",
-            }}
           />
-        </div>
+        </ImageContainer>
 
-        <p
-          style={{
-            fontSize: "4.25rem",
-            fontWeight: "bold",
-            color: "#333",
-            marginTop: "3rem",
-            fontFamily: "'Marker Felt', Fantasy",
-            marginBottom: "1rem",
-          }}
-        >
-          PAGE NOT FOUND
-        </p>
-        <p
-          style={{
-            fontSize: "1.7rem",
-            color: "#333",
-            fontWeight: "bold",
-            fontFamily: "'Marker Felt', Fantasy",
-          }}
-        >
+        <Heading>PAGE NOT FOUND</Heading>
+        <SubHeading>
           SORRY, THE PAGE YOU ARE LOOKING FOR COULD NOT BE FOUND
-        </p>
+        </SubHeading>
 
-        <Link
-          href="/"
-          style={{
-            fontSize: "1rem",
-            display: "inline-block",
-            backgroundColor: "#007bff",
-            color: "#fff",
-            padding: "0.5rem 1rem",
-            borderRadius: "0.25rem",
-            transition: "background-color 0.3s ease",
-            marginTop: "1rem",
-            textDecoration: "none",
-            fontFamily: "'Marker Felt', Fantasy",
-          }}
-        >
-          Go to Home
-        </Link>
-      </div>
-    </div>
+        <div>
+          <StyledLink href="/">Go to Home</StyledLink>
+        </div>
+      </Content>
+    </Container>
   );
 }
 
-export default Custom404;
+export default dynamic(() => Promise.resolve(Custom404), { ssr: false });

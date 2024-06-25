b2b05af2ac72953fc6c35ba792b733d7d1c65e04
Ktbch <140395978+Ktbch@users.noreply.github.com>
2024-06-25 14:53:13 +0200

    Fix overflow and ensure visibility of action buttons in WidgetHeader component (#78)

--- src/frontend/views/Dashboard/Widget/_components/WidgetHeader/index.tsx
+++ src/frontend/views/Dashboard/Widget/_components/WidgetHeader/index.tsx
@@ -35,13 +35,15 @@ export function WidgetHeader({
 
   return (
     <div className="flex justify-between items-start w-full">
-      <div className="flex flex-1 w-full gap-2">
+       <div className="flex flex-1 items-center overflow-hidden w-full gap-2">
         {setting && (
           <SortableKnob>
             <GrabIcon width={18} />
           </SortableKnob>
         )}
-        <p className="truncate w-[calc(100%-1px)]">{title} TODO</p>
+          <p className="truncate max-w-[calc(100%-5px)] w-full ">
+          {title} TODO DONE
+        </p>
       </div>
       {setting ? (
         <ActionButtons
